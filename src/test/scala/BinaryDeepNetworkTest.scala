f8ff910e25bd4291327b8bf2d4b5a0aa56fb8262
VzxPLnHqr <VzxPLnHqr@proton.me>
2024-08-14 22:36:11 +0000

    testing uniform xavier initialization

--- src/test/scala/BinaryDeepNetworkTest.scala
+++ src/test/scala/BinaryDeepNetworkTest.scala
@@ -6,7 +6,7 @@ import synapses.lib._
 class BinaryDeepNetworkTest:
 
   // a "somewhat deep" network with 37 hidden layers and a single output
-  val layerSizesLarge = List(1024) ++ List.fill(37)(6) ++ List(1)
+  val layerSizesLarge = List(1024) ++ List.fill(67)(6) ++ List(1)
 
   // same input and output layers but with only one hidden layer
   val layerSizesSmall= List(1024) ++ List(6) ++ List(1)
@@ -22,19 +22,40 @@ class BinaryDeepNetworkTest:
 
   def activationF(layerIndex: Int): Fun =
     layerIndex match
-      case _ => Fun.sigmoid
+      case _ => Fun.leakyReLU
 
-  def weightInitF(_layerIndex: Int): Double =
+  def weightInitFlarge( layerIndex: Int): Double =
+    // Uniform Xavier Initialization
+    // https://365datascience.com/tutorials/machine-learning-tutorials/what-is-xavier-initialization/
+
+    // get the number of inputs and outputs for the layer in question
+    val (num_inputs, num_outputs) = layerIndex match 
+      case 0 => 
+        // input layer always has same number of inputs and outputs
+        (layerSizesLarge(0),layerSizesLarge(0))
+      case i if( i == layerSizesLarge.size - 1 ) => 
+        // output layer has inputs, but no outputs
+        (layerSizesLarge(layerIndex - 1), 0)
+      case i => 
+        // inner layers have num inputs from prev layer
+        (layerSizesLarge(layerIndex - 1), layerSizesLarge(layerIndex))
+    
+    // return weight selected uniformly from [-x,x]
+    val x = math.sqrt(6.0 / (num_inputs + num_outputs))
+    rand.between(-x,x)
+
+
+  def weightInitFsmall(_layerIndex: Int): Double =
     1.0 - 2.0 * rand.nextDouble()
 
-  val largeNet = Net(layerSizesLarge, activationF, weightInitF)
-  val smallNet = Net(layerSizesSmall, activationF, weightInitF)
+  val largeNet = Net(layerSizesLarge, activationF, weightInitFlarge)
+  val smallNet = Net(layerSizesSmall, activationF, weightInitFsmall)
 
   @Test def `inputs not equal`(): Unit = 
     assertNotEquals(input1, input2)
 
-  @Test def `same exact output for different inputs`(): Unit =
-    assertEquals(
+  @Test def `different inputs should give different outputs`(): Unit =
+    assertNotEquals(
       largeNet.parPredict(input1),
       largeNet.parPredict(input2)
     )
